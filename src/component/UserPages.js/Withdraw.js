b1c0cd310959f38cf2a066c5abdaa4dfc478b44d
Camillarhi <rhitanene@gmail.com>
2022-05-24 11:58:09 +0100

    init

--- src/component/UserPages.js/Withdraw.js
+++ src/component/UserPages.js/Withdraw.js
@@ -6,6 +6,15 @@ export default function Withdraw() {
     <>
     <h1 className='text-dark'>Withdraw</h1>
     <TradingView />
+   <div className="card shadow mb-4">
+  <div className="card-header py-3">
+    <h6 className="m-0 font-weight-bold text-primary">Basic Card Example</h6>
+  </div>
+  <div className="card-body">
+    Wallet Address
+  </div>
+</div>
+
     </>
   )
 }
