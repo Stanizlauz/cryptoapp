1e862f4889b21145ed20c2c00fd32810dd613042
Stanizlauz <izukachibuzor4@gmail.com>
2022-06-05 22:51:19 +0100

    sun

--- src/component/UserPages.js/DepositCards.js
+++ src/component/UserPages.js/DepositCards.js
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 
 export default function DepositCards({ image, coin }) {
-
-  const [value, setValue] = useState("");
-  const [copied, setCopied] = useState(false) 
+  const [value, setValue] = useState(
+    "bc1qeyvn3dl5a2tjrhd0wfc62vkld4sefnq2zd3vs5"
+  );
+  const [copied, setCopied] = useState(false);
 
   return (
     <>
@@ -57,32 +58,67 @@ export default function DepositCards({ image, coin }) {
                 </button>
               </div>
               <div className="modal-body">
-                <h5
+                <div className="form-outline mb-4 text-dark">
+                  Enter Amount
+                  <input
+                    type="number"
+                    placeholder="Eg.2500 (Min $500)"
+                    id="form1Example1"
+                    className="form-control"
+                  />
+                </div>
+                <hr />
+                <div className="">
+                  <textarea
+                    rows={1}
+                    cols={50}
+                    style={{ maxWidth: "100%" }}
+                    className=" text-dark"
+                    defaultValue={value}
+                  />
+                </div>
+                <div className="mb-1 ml-3">
+                  <small className="text-info pointer" id="text_infild">
+                    Copy address to clipboard{" "}
+                    <CopyToClipboard
+                      value={value}
+                      onCopy={() => setCopied(true)}
+                    >
+                      <i className="fas fa-copy text-dark ml-2" />
+                    </CopyToClipboard>
+                    {copied ? (
+                      <span className="text-success ml-2">Copied.</span>
+                    ) : null}
+                  </small>
+                </div>
+                <hr />
+                <div className="mt-3 mb-3 text-dark">
+                  In order to confirm payment. A proof of payment have to be
+                  uploaded.
+                </div>
+                <div className="form-group">
+                  <label className="text-dark" htmlFor="picture">Upload proof of payment</label>
+                  <input
+                    className="text-dark"
+                    id="picture"
+                    name="picture"
+                    type="file"
+                  />
+                </div>
+                {/* <h5
                   value={value}
-                //   onChange={({ target: { value } }) =>
-                //     setValue({ value, setCopied: false })
-                //   }
+              
                   className="text-dark"
                 >
                   btc5463jwkwiwih
                   <CopyToClipboard
                     text={value}
-                    onCopy={() => setCopied( true)}
-                  >
+                    onCopy={() => setCopied( true)}>
                     <i className="fas fa-copy text-dark ml-2" />
                   </CopyToClipboard>
                   {copied ? <span className="text-success ml-2">Copied.</span> : null}
 
-                </h5>
-                <div className="form-outline mb-4">
-                  <input
-                    type="number"
-                    placeholder="Amount"
-                    id="form1Example1"
-                    className="form-control"
-                  />
-                  {/* <label className="form-label">Wallet address</label> */}
-                </div>
+                </h5> */}
               </div>
               <div className="modal-footer">
                 <button
